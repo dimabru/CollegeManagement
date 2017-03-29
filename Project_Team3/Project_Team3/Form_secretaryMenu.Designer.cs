cd9ff3d8470c68f47021c2b251d2859a3f6b04cd
Jonathan Leon <johnleon24@gmail.com>
2017-03-29 12:56:30 +0200

    Added global message to secretary menu

--- Project_Team3/Project_Team3/Form_secretaryMenu.Designer.cs
+++ Project_Team3/Project_Team3/Form_secretaryMenu.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.logoutButton = new System.Windows.Forms.Button();
+            this.globalMessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // logoutButton
@@ -40,21 +41,33 @@
             this.logoutButton.Text = "Log out";
             this.logoutButton.UseVisualStyleBackColor = true;
             // 
+            // globalMessage
+            // 
+            this.globalMessage.AutoSize = true;
+            this.globalMessage.Location = new System.Drawing.Point(12, 12);
+            this.globalMessage.Name = "globalMessage";
+            this.globalMessage.Size = new System.Drawing.Size(193, 13);
+            this.globalMessage.TabIndex = 1;
+            this.globalMessage.Text = "Global Message Will be Displayed Here";
+            // 
             // Form_secretaryMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.globalMessage);
             this.Controls.Add(this.logoutButton);
             this.Name = "Form_secretaryMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Form_secretaryMenu";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Label globalMessage;
     }
 }
