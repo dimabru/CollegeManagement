2d05133a8ffd0f1056dd8afcd15679752f7aad93
Isabelle135 <isabelle.m135@gmail.com>
2017-03-29 13:56:56 +0300

    Added student's menu forms

---
+++ Project_Team3/Project_Team3/Form_studentAssociationMenu.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Project_Team3
+{
+    partial class Form_studentAssociationMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Association information";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Register to association events";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Unregister association events";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(62, 298);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(210, 42);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Back to last menu";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form_studentAssociationMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 436);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form_studentAssociationMenu";
+            this.Text = "Student\'s Association Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
