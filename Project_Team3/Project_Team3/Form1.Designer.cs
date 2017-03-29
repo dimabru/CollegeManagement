eed865472d166c5f47638b17363920624d26bccf
Liran Rotenberg <liranr23@gmail.com>
2017-03-29 12:39:06 +0300

    Added login gui, login query

--- Project_Team3/Project_Team3/Form1.Designer.cs
+++ Project_Team3/Project_Team3/Form1.Designer.cs
@@ -29,33 +29,102 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 12);
+            this.button1.Location = new System.Drawing.Point(75, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 23);
+            this.button1.Size = new System.Drawing.Size(304, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Check connection to database";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(145, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 49);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 261);
+            this.ClientSize = new System.Drawing.Size(442, 402);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Project_Team3/Project_Team3/Form1.cs
+++ Project_Team3/Project_Team3/Form1.cs
@@ -17,7 +17,8 @@ namespace Project_Team3
     {
 
         private DBconnect db;
-
+        private string username;
+        private string password;
         public Form1()
         {
             InitializeComponent();
@@ -29,6 +30,50 @@ namespace Project_Team3
         {
             db = new DBconnect();
             db.OpenConn();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            username = this.textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = this.textBox2.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            db = new DBconnect();
+            db.OpenConn();
+            getInfo();
+            db.CloseConn(db.ConnStatus());
+        }
+        private void getInfo() {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = db.getConnection();
+            cmd.CommandText = "SELECT ID FROM USERS WHERE USERNAME = @username and PASS = @password";
+            cmd.Parameters.AddWithValue("username", username);
+            cmd.Parameters.AddWithValue("password", password);
+            SqlDataAdapter sda = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            sda.SelectCommand = cmd;
+            try
+            {
+
+                sda.Fill(ds, "Check");
+                MessageBox.Show("ID: " + Convert.ToInt32(ds.Tables[0].Rows[0].ItemArray[0].ToString()));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Incorrect Data!");
+            }
         }
     }
 }
