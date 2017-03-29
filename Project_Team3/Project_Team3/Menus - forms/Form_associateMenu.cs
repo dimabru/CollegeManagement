bf838cfb01bb1b582c0ca44df70b63dca4dc519a
Alexey Silyuk <alexesi@ac.sce.ac.il>
2017-03-29 16:13:06 +0300

    added associate menu

---
+++ Project_Team3/Project_Team3/Menus - forms/Form_associateMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Team3.Menus___forms
+{
+    public partial class Form_associateMenu : Form
+    {
+        public Form_associateMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
