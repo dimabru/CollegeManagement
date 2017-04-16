3c39cb3fa515b9b6eac112772a1263466854ebc0
dimab <dimabru@gmail.com>
2017-04-16 14:20:33 +0300

    Create class Room and arrange folders

---
+++ Project_Team3/Project_Team3/Courses/Room.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Team3
+{
+    class Room
+    {
+
+        private bool board, projector;
+        private int maxStudents;
+        private String location;
+
+        public Room(bool brd, bool proj, int max, String loc)
+        {
+            board = brd;
+            projector = proj;
+            maxStudents = max;
+            location = loc;
+        }
+
+        public bool getBoard() { return board; }
+        public bool getProjector() { return projector; }
+        public int getMaxStudents() { return maxStudents; }
+        public String getLocation() { return location; }
+
+        public void setBoard(bool b) { board = b; }
+        public void setProjector(bool p) { projector = p; }
+        public bool setMaxStudents(int max)
+        {
+            if (max < 1) return false;
+            maxStudents = max;
+            return true;
+        }
+        public bool setLocation(String loc)
+        {
+            if (loc.Equals("")) return false;
+            location = loc;
+            return true;
+        }
+    }
+}
