fffa29b354f06b205b8617884d7b389aeb49a910
Jonathan Leon <johnleon24@gmail.com>
2017-04-24 18:55:29 +0200

    Changed new forms' size

--- Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryManageStudent.Designer.cs
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryManageStudent.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form_secretaryManageStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 517);
+            this.Name = "Form_secretaryManageStudent";
             this.Text = "Form_secretaryManageStudent";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
