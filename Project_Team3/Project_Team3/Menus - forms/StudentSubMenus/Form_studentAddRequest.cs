3effb709df6e479fdff0e9fca3a09ff653e97473
Alexey Silyuk <alexesi@ac.sce.ac.il>
2017-04-24 09:56:40 +0300

    added form to add new student request by student

---
+++ Project_Team3/Project_Team3/Menus - forms/StudentSubMenus/Form_studentAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Team3.Menus___forms.StudentSubMenus
+{
+    public partial class Form_studentAddRequest : Form
+    {
+        public Form_studentAddRequest()
+        {
+            InitializeComponent();
+        }
+    }
+}
