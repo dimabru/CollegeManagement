761218f2af98c15a60faae5a0b5e1abc873ef9dd
Isabelle135 <isabelle.m135@gmail.com>
2017-05-03 16:50:44 +0300

    assignments 61, 61, 47. new association form for secetary. last one deleted!

---
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryAssociationEventsNew.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Project_Team3.Menus___forms.SecretarySubMenus
+{
+    partial class Form_secretaryAssociationEventsNew
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.associationEventsGrid = new System.Windows.Forms.DataGridView();
+            this.RefreshData = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.associationEventsGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // associationEventsGrid
+            // 
+            this.associationEventsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.associationEventsGrid.Location = new System.Drawing.Point(192, 104);
+            this.associationEventsGrid.Name = "associationEventsGrid";
+            this.associationEventsGrid.RowTemplate.Height = 24;
+            this.associationEventsGrid.Size = new System.Drawing.Size(500, 254);
+            this.associationEventsGrid.TabIndex = 0;
+            this.associationEventsGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.associationEventsGrid_CellContentClick);
+            // 
+            // RefreshData
+            // 
+            this.RefreshData.Location = new System.Drawing.Point(376, 402);
+            this.RefreshData.Name = "RefreshData";
+            this.RefreshData.Size = new System.Drawing.Size(141, 37);
+            this.RefreshData.TabIndex = 1;
+            this.RefreshData.Text = "Refresh Data";
+            this.RefreshData.UseVisualStyleBackColor = true;
+            this.RefreshData.Click += new System.EventHandler(this.RefreshData_Click);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(376, 463);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(141, 37);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "Back to last menu";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // Form_secretaryAssociationEventsNew
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 631);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.RefreshData);
+            this.Controls.Add(this.associationEventsGrid);
+            this.Name = "Form_secretaryAssociationEventsNew";
+            this.Text = "Form_secretaryAssociationEvents";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_secretaryAssociationEventsNew_FormClosed);
+            this.Load += new System.EventHandler(this.Form_secretaryAssociationEventsNew_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.associationEventsGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView associationEventsGrid;
+        private System.Windows.Forms.Button RefreshData;
+        private System.Windows.Forms.Button BackButton;
+    }
+}
---
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryAssociationEventsNew.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Project_Team3.Menus___forms.SecretarySubMenus
+{
+    public partial class Form_secretaryAssociationEventsNew : Form
+    {
+        private DataSet allEvents;
+
+        public Form_secretaryAssociationEventsNew()
+        {
+            InitializeComponent();
+        }
+
+        private void associationEventsGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void retreiveData()
+        {
+            DBconnect db = new DBconnect();
+            db.OpenConn();
+
+            //create data adapter
+            var dataAdapter = new SqlDataAdapter("select EVENT_NAME from Event", db.ConnectionStringGet());
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            //try to fill sataset with query result
+            try
+            {
+                dataAdapter.Fill(ds);
+                associationEventsGrid.ReadOnly = true;
+                associationEventsGrid.DataSource = ds.Tables[0];
+                allEvents = ds;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Query not returned any data");
+            }
+
+            db.CloseConn(db.ConnStatus());
+        }
+
+        private void RefreshData_Click(object sender, EventArgs e)
+        {
+            retreiveData();
+        }
+
+        private void Form_secretaryAssociationEventsNew_Load(object sender, EventArgs e)
+        {
+            retreiveData();
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form_secretaryAssociationEventsNew_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form_secretaryMenu parent = (Form_secretaryMenu)this.Owner;
+            parent.Show();
+        }
+    }
+}
