ef9d0f824686821b7e87229cb692a283bc7266e2
Isabelle135 <isabelle.m135@gmail.com>
2017-05-06 15:17:00 +0300

    B_USER_STORY2_Admin can delete Student _12

---
+++ Project_Team3/Project_Team3/Menus - forms/AdminSubMenus/adminRemoveAssociate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Team3.Menus___forms.AdminSubMenus
+{
+    public partial class adminRemoveAssociate : Form
+    {
+        public adminRemoveAssociate()
+        {
+            InitializeComponent();
+        }
+    }
+}
