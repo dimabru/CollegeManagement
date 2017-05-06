6034a8eece88ec9e2e1498e6bc2108401c35499b
Isabelle135 <isabelle.m135@gmail.com>
2017-05-06 15:48:45 +0300

    B_USER_STORY12_Admin remove Associate_42

--- Project_Team3/Project_Team3/Menus - forms/AdminSubMenus/adminRemoveAssociate.Designer.cs
+++ Project_Team3/Project_Team3/Menus - forms/AdminSubMenus/adminRemoveAssociate.Designer.cs
@@ -28,19 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.backButton = new System.Windows.Forms.Button();
+            this.instructonLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.assocLst = new System.Windows.Forms.Button();
+            this.removeAssoc = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(204, 287);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(129, 36);
+            this.backButton.TabIndex = 9;
+            this.backButton.Text = "Back to last menu";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // instructonLabel
+            // 
+            this.instructonLabel.AutoSize = true;
+            this.instructonLabel.Location = new System.Drawing.Point(121, 102);
+            this.instructonLabel.Name = "instructonLabel";
+            this.instructonLabel.Size = new System.Drawing.Size(314, 17);
+            this.instructonLabel.TabIndex = 6;
+            this.instructonLabel.Text = "Enter the ID of the associate you want to remove";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // assocLst
+            // 
+            this.assocLst.Location = new System.Drawing.Point(204, 241);
+            this.assocLst.Name = "assocLst";
+            this.assocLst.Size = new System.Drawing.Size(129, 36);
+            this.assocLst.TabIndex = 10;
+            this.assocLst.Text = "Associate List";
+            this.assocLst.UseVisualStyleBackColor = true;
+            this.assocLst.Click += new System.EventHandler(this.assocLst_Click);
+            // 
+            // removeAssoc
+            // 
+            this.removeAssoc.Location = new System.Drawing.Point(192, 190);
+            this.removeAssoc.Name = "removeAssoc";
+            this.removeAssoc.Size = new System.Drawing.Size(154, 34);
+            this.removeAssoc.TabIndex = 11;
+            this.removeAssoc.Text = "Remove Associate";
+            this.removeAssoc.UseVisualStyleBackColor = true;
+            this.removeAssoc.Click += new System.EventHandler(this.removeAssoc_Click);
             // 
             // adminRemoveAssociate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(542, 519);
+            this.Controls.Add(this.removeAssoc);
+            this.Controls.Add(this.assocLst);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.instructonLabel);
+            this.Controls.Add(this.textBox1);
             this.Name = "adminRemoveAssociate";
             this.Text = "adminRemoveAssociate";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.adminRemoveAssociate_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button associateList;
+        private System.Windows.Forms.Label instructonLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button assocLst;
+        private System.Windows.Forms.Button removeAssoc;
     }
 }
--- Project_Team3/Project_Team3/Menus - forms/AdminSubMenus/adminRemoveAssociate.cs
+++ Project_Team3/Project_Team3/Menus - forms/AdminSubMenus/adminRemoveAssociate.cs
@@ -16,5 +16,64 @@ namespace Project_Team3.Menus___forms.AdminSubMenus
         {
             InitializeComponent();
         }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void adminRemoveAssociate_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form_adminManageAccountMenu parent = (Form_adminManageAccountMenu)this.Owner;
+            parent.Show();
+        }
+
+        private void assocLst_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("UNDER CONSTRUCTION");
+        }
+
+        private void removeAssoc_Click(object sender, EventArgs e)
+        {
+            DBconnect db = new DBconnect();
+            string idToRemove = this.textBox1.Text;
+            string commandString = "DELETE FROM Users WHERE Users.ID = " + idToRemove;
+            if (checkFieldText())
+            {
+                db.OpenConn();
+                db.executionQuery(commandString);
+                MessageBox.Show("User Deleted.\n");
+            }
+        }
+
+        private bool checkFieldText()
+        {
+            Project_Team3.Classes.FunctionsForAllProgram funcs = new Classes.FunctionsForAllProgram();
+            int idInt = -1;
+
+            if (string.IsNullOrWhiteSpace(this.textBox1.Text))
+            {
+                MessageBox.Show("The ID field cannot be empty.\n");
+                return false;
+            }
+
+            try
+            {
+                idInt = Convert.ToInt32(this.textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("ID must contain numerical digits only.");
+                return false;
+            }
+
+            if (!(funcs.ifUserIDinDatabase(idInt)))
+            {
+                MessageBox.Show("Such user doesn't exist.\n");
+                return false;
+            }
+
+            return true;
+        }
     }
 }
