6009a86f58ef2061c10114e4794412300b372c31
Jonathan Leon <johnleon24@gmail.com>
2017-05-07 21:20:54 +0200

    Started Requirement for secretary to add student

--- Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryManageStudent.Designer.cs
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryManageStudent.Designer.cs
@@ -29,11 +29,19 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.semesterLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.backButton = new System.Windows.Forms.Button();
+            this.addCoursesLabel = new System.Windows.Forms.Label();
+            this.coursesDataGridView = new System.Windows.Forms.DataGridView();
+            this.addCourseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.coursesDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(288, 133);
+            this.button1.Location = new System.Drawing.Point(448, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(146, 43);
             this.button1.TabIndex = 0;
@@ -41,22 +49,112 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.CoursesReport_Click);
             // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.nameLabel.Location = new System.Drawing.Point(12, 13);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(59, 20);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name: ";
+            // 
+            // semesterLabel
+            // 
+            this.semesterLabel.AutoSize = true;
+            this.semesterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.semesterLabel.Location = new System.Drawing.Point(12, 81);
+            this.semesterLabel.Name = "semesterLabel";
+            this.semesterLabel.Size = new System.Drawing.Size(86, 20);
+            this.semesterLabel.TabIndex = 2;
+            this.semesterLabel.Text = "Semester: ";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.usernameLabel.Location = new System.Drawing.Point(12, 46);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(91, 20);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Username: ";
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(610, 3);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(123, 43);
+            this.backButton.TabIndex = 4;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // addCoursesLabel
+            // 
+            this.addCoursesLabel.AutoSize = true;
+            this.addCoursesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.addCoursesLabel.Location = new System.Drawing.Point(290, 148);
+            this.addCoursesLabel.Name = "addCoursesLabel";
+            this.addCoursesLabel.Size = new System.Drawing.Size(132, 25);
+            this.addCoursesLabel.TabIndex = 5;
+            this.addCoursesLabel.Text = "Add courses";
+            // 
+            // coursesDataGridView
+            // 
+            this.coursesDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.coursesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.coursesDataGridView.ColumnHeadersVisible = false;
+            this.coursesDataGridView.Location = new System.Drawing.Point(0, 195);
+            this.coursesDataGridView.MultiSelect = false;
+            this.coursesDataGridView.Name = "coursesDataGridView";
+            this.coursesDataGridView.ReadOnly = true;
+            this.coursesDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.coursesDataGridView.Size = new System.Drawing.Size(733, 310);
+            this.coursesDataGridView.TabIndex = 6;
+            // 
+            // addCourseButton
+            // 
+            this.addCourseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.addCourseButton.Location = new System.Drawing.Point(518, 138);
+            this.addCourseButton.Name = "addCourseButton";
+            this.addCourseButton.Size = new System.Drawing.Size(206, 51);
+            this.addCourseButton.TabIndex = 7;
+            this.addCourseButton.Text = "Add Course";
+            this.addCourseButton.UseVisualStyleBackColor = true;
+            this.addCourseButton.Click += new System.EventHandler(this.addCourseButton_Click);
+            // 
             // Form_secretaryManageStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 517);
+            this.Controls.Add(this.addCourseButton);
+            this.Controls.Add(this.coursesDataGridView);
+            this.Controls.Add(this.addCoursesLabel);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.semesterLabel);
+            this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.button1);
             this.Name = "Form_secretaryManageStudent";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form_secretaryManageStudent";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_secretaryManageStudent_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.coursesDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label semesterLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Label addCoursesLabel;
+        private System.Windows.Forms.DataGridView coursesDataGridView;
+        private System.Windows.Forms.Button addCourseButton;
     }
 }
--- Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryManageStudent.cs
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryManageStudent.cs
@@ -8,21 +8,115 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Project_Team3.Classes;
+using System.Data.SqlClient;
 
 namespace Project_Team3.Menus___forms.SecretarySubMenus
 {
     public partial class Form_secretaryManageStudent : Form
     {
         private Student student;
+        private DBconnect connection;
+        private SqlCommand selectCoursesCommand;
+        private SqlCommand insertScheduleCommand;
+        private SqlCommand selectSchedulesCommand;
 
         public Form_secretaryManageStudent(Student student)
         {
             this.student = student;
-            MessageBox.Show(student.Username);
             InitializeComponent();
+            nameLabel.Text += student.Name + " " + student.Surename;
+            usernameLabel.Text += student.Username;
+            semesterLabel.Text += student.Semester;
+            Init();
+            UpdateCoursesDataGridView();
+        }
+
+        private void Init()
+        {
+            connection = new DBconnect();
+
+            selectCoursesCommand = new SqlCommand();
+            selectCoursesCommand.CommandType = CommandType.Text;
+            selectCoursesCommand.Connection = connection.getConnection();
+            selectCoursesCommand.CommandText = @"
+                                                SELECT * FROM Course  
+                                                WHERE COURSE_SEMESTER = @semester
+                                                EXCEPT 
+                                                SELECT Course.* FROM Course , Schedules
+                                                WHERE Course.COURSE_ID=Schedules.COURSE_ID AND STUDENT_UN = @username
+                                               ";
+            selectCoursesCommand.Parameters.AddWithValue("semester", student.Semester);
+            selectCoursesCommand.Parameters.AddWithValue("username", student.Username);
+
+            selectSchedulesCommand = new SqlCommand();
+            selectSchedulesCommand.CommandType = CommandType.Text;
+            selectSchedulesCommand.Connection = connection.getConnection();
+            selectSchedulesCommand.CommandText = @"
+                                                    SELECT Schedules.COURSE_ID FROM Schedules 
+                                                    WHERE STUDENT_UN=@username 
+                                                    EXCEPT 
+                                                    SELECT Schedules.COURSE_ID FROM Schedules,Course
+                                                    WHERE
+                                                    STUDENT_UN=@username
+                                                    AND
+                                                    Course.Course_ID=Schedules.COURSE_ID
+                                                    AND
+                                                    (
+                                                    @day != COURSE_DAY
+                                                    OR
+                                                    (@end < START_HOUR AND @start < START_HOUR)
+                                                    OR
+                                                    (@end > END_HOUR AND @start > END_HOUR)
+                                                    )
+                                                    
+                                                 ";
+                                                    
+            insertScheduleCommand = new SqlCommand();
+            insertScheduleCommand.CommandType = CommandType.Text;
+            insertScheduleCommand.Connection = connection.getConnection();
+            insertScheduleCommand.CommandText = "INSERT INTO Schedules VALUES(@username,@courseid)";
             
         }
 
+        private void UpdateCoursesDataGridView()
+        {
+            try
+            {
+                DataSet ds = connection.generalCommand(selectCoursesCommand);
+                if (ds.Tables.Count > 0)
+                {
+                    coursesDataGridView.AutoGenerateColumns = true;
+                    coursesDataGridView.DataSource = ds.Tables[0];
+                }
+                else
+                {
+                    MessageBox.Show("No Record Found");
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private int AddCourseToSchedule(int startHour, int endHour ,int courseID , string day)
+        {
+            selectSchedulesCommand.Parameters.Clear();
+            selectSchedulesCommand.Parameters.AddWithValue("username", student.Username);
+            selectSchedulesCommand.Parameters.AddWithValue("start", startHour);
+            selectSchedulesCommand.Parameters.AddWithValue("end", endHour);
+            selectSchedulesCommand.Parameters.AddWithValue("day", day);
+
+            DataSet ds = connection.generalCommand(selectSchedulesCommand);
+
+            if (ds.Tables[0].Rows.Count != 0) MessageBox.Show("Course overlapping with another course or event!");
+
+            MessageBox.Show(courseID + " " + startHour + " " + endHour);
+
+
+            return -1;
+        }
         private void Form_secretaryManageStudent_FormClosed(object sender, FormClosedEventArgs e)
         {
             Form_secretaryStudentSearch parent = (Form_secretaryStudentSearch)this.Owner;
@@ -37,6 +131,21 @@ namespace Project_Team3.Menus___forms.SecretarySubMenus
                 this.Hide();
                 manageStudentCourses.ShowDialog(this);
             }
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addCourseButton_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(coursesDataGridView.SelectedRows[0].Cells[0].Value);
+            string day = coursesDataGridView.SelectedRows[0].Cells[5].Value.ToString();
+            int startHour = Convert.ToInt32(coursesDataGridView.SelectedRows[0].Cells[6].Value);
+            int endHour = Convert.ToInt32(coursesDataGridView.SelectedRows[0].Cells[7].Value);
+
+            AddCourseToSchedule(startHour, endHour, id , day);
         }
     }
 }
