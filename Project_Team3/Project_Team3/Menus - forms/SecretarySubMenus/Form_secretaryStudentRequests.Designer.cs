3e337e9a40b9e031277798d2b459bdabb6c802b1
Liran Rotenberg <liranr23@gmail.com>
2017-05-07 22:39:29 +0300

    32_secretary_Watch student requests

--- Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryStudentRequests.Designer.cs
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryStudentRequests.Designer.cs
@@ -33,17 +33,18 @@
             this.denyRequestButton = new System.Windows.Forms.Button();
             this.detailsRequestButton = new System.Windows.Forms.Button();
             this.studentRequestsLabel = new System.Windows.Forms.Label();
-            this.requestsListBox = new System.Windows.Forms.ListBox();
+            this.StudentReqGrid = new System.Windows.Forms.DataGridView();
+            this.BackButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentReqGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // acceptRequestButton
             // 
             this.acceptRequestButton.BackColor = System.Drawing.Color.MistyRose;
             this.acceptRequestButton.Font = new System.Drawing.Font("David", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.acceptRequestButton.Location = new System.Drawing.Point(693, 191);
-            this.acceptRequestButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.acceptRequestButton.Location = new System.Drawing.Point(709, 209);
             this.acceptRequestButton.Name = "acceptRequestButton";
-            this.acceptRequestButton.Size = new System.Drawing.Size(235, 46);
+            this.acceptRequestButton.Size = new System.Drawing.Size(176, 37);
             this.acceptRequestButton.TabIndex = 1;
             this.acceptRequestButton.Text = "Accept";
             this.acceptRequestButton.UseVisualStyleBackColor = false;
@@ -53,10 +54,9 @@
             // 
             this.denyRequestButton.BackColor = System.Drawing.Color.MistyRose;
             this.denyRequestButton.Font = new System.Drawing.Font("David", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.denyRequestButton.Location = new System.Drawing.Point(693, 270);
-            this.denyRequestButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.denyRequestButton.Location = new System.Drawing.Point(709, 312);
             this.denyRequestButton.Name = "denyRequestButton";
-            this.denyRequestButton.Size = new System.Drawing.Size(235, 46);
+            this.denyRequestButton.Size = new System.Drawing.Size(176, 37);
             this.denyRequestButton.TabIndex = 2;
             this.denyRequestButton.Text = "Deny";
             this.denyRequestButton.UseVisualStyleBackColor = false;
@@ -66,10 +66,9 @@
             // 
             this.detailsRequestButton.BackColor = System.Drawing.Color.MistyRose;
             this.detailsRequestButton.Font = new System.Drawing.Font("David", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.detailsRequestButton.Location = new System.Drawing.Point(693, 343);
-            this.detailsRequestButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.detailsRequestButton.Location = new System.Drawing.Point(709, 410);
             this.detailsRequestButton.Name = "detailsRequestButton";
-            this.detailsRequestButton.Size = new System.Drawing.Size(235, 46);
+            this.detailsRequestButton.Size = new System.Drawing.Size(176, 37);
             this.detailsRequestButton.TabIndex = 3;
             this.detailsRequestButton.Text = "Details";
             this.detailsRequestButton.UseVisualStyleBackColor = false;
@@ -79,48 +78,56 @@
             // 
             this.studentRequestsLabel.AutoSize = true;
             this.studentRequestsLabel.Font = new System.Drawing.Font("David", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.studentRequestsLabel.Location = new System.Drawing.Point(397, 142);
-            this.studentRequestsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.studentRequestsLabel.Location = new System.Drawing.Point(298, 115);
             this.studentRequestsLabel.Name = "studentRequestsLabel";
-            this.studentRequestsLabel.Size = new System.Drawing.Size(230, 28);
+            this.studentRequestsLabel.Size = new System.Drawing.Size(181, 21);
             this.studentRequestsLabel.TabIndex = 4;
             this.studentRequestsLabel.Text = "Student Requests:";
             // 
-            // requestsListBox
+            // StudentReqGrid
             // 
-            this.requestsListBox.Font = new System.Drawing.Font("David", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.requestsListBox.FormattingEnabled = true;
-            this.requestsListBox.ItemHeight = 17;
-            this.requestsListBox.Items.AddRange(new object[] {
-            "Jonathan:Hedva 2 hours",
-            "Isabelle:\tLinear Algebra cancel",
-            "Alexei:\tAlgorithms switch",
-            "Liran:\tDatabases change"});
-            this.requestsListBox.Location = new System.Drawing.Point(376, 202);
-            this.requestsListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.requestsListBox.Name = "requestsListBox";
-            this.requestsListBox.Size = new System.Drawing.Size(283, 174);
-            this.requestsListBox.TabIndex = 5;
+            this.StudentReqGrid.AllowUserToAddRows = false;
+            this.StudentReqGrid.AllowUserToDeleteRows = false;
+            this.StudentReqGrid.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.StudentReqGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentReqGrid.Location = new System.Drawing.Point(60, 180);
+            this.StudentReqGrid.MultiSelect = false;
+            this.StudentReqGrid.Name = "StudentReqGrid";
+            this.StudentReqGrid.ReadOnly = true;
+            this.StudentReqGrid.Size = new System.Drawing.Size(609, 302);
+            this.StudentReqGrid.TabIndex = 5;
+            // 
+            // BackButton
+            // 
+            this.BackButton.BackColor = System.Drawing.Color.MistyRose;
+            this.BackButton.Font = new System.Drawing.Font("David", 13.8F, System.Drawing.FontStyle.Bold);
+            this.BackButton.Location = new System.Drawing.Point(418, 520);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(176, 37);
+            this.BackButton.TabIndex = 6;
+            this.BackButton.Text = "Go Back";
+            this.BackButton.UseVisualStyleBackColor = false;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
             // Form_secretaryStudentRequests
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1350, 750);
-            this.Controls.Add(this.requestsListBox);
+            this.ClientSize = new System.Drawing.Size(1012, 609);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.StudentReqGrid);
             this.Controls.Add(this.studentRequestsLabel);
             this.Controls.Add(this.detailsRequestButton);
             this.Controls.Add(this.denyRequestButton);
             this.Controls.Add(this.acceptRequestButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form_secretaryStudentRequests";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Student Requests";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_secretaryStudentRequests_FormClosed);
-            this.Load += new System.EventHandler(this.Form_secretaryStudentRequests_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentReqGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,6 +138,7 @@
         private System.Windows.Forms.Button denyRequestButton;
         private System.Windows.Forms.Button detailsRequestButton;
         private System.Windows.Forms.Label studentRequestsLabel;
-        private System.Windows.Forms.ListBox requestsListBox;
+        private System.Windows.Forms.DataGridView StudentReqGrid;
+        private System.Windows.Forms.Button BackButton;
     }
 }
--- Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryStudentRequests.cs
+++ Project_Team3/Project_Team3/Menus - forms/SecretarySubMenus/Form_secretaryStudentRequests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,39 @@ namespace Project_Team3.Menus___forms.SecretarySubMenus
 {
     public partial class Form_secretaryStudentRequests : Form
     {
+        DBconnect connection;
+        SqlCommand command;
         public Form_secretaryStudentRequests()
         {
             InitializeComponent();
+            Init();
+            UpdateGrid();
+        }
+        private void Init()
+        {
+            connection = new DBconnect();
+            command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.Connection = connection.getConnection();
+            connection = new DBconnect();
+            command.CommandText = "SELECT * FROM StudentRequests";
+        }
+
+        private void UpdateGrid()
+        {
+            try
+            {
+                DataSet ds = connection.generalCommand(command);
+                if (ds.Tables.Count > 0)
+                {
+                    StudentReqGrid.AutoGenerateColumns = true;
+                    StudentReqGrid.DataSource = ds.Tables[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
 
         private void Form_secretaryStudentRequests_FormClosed(object sender, FormClosedEventArgs e)
@@ -39,9 +70,9 @@ namespace Project_Team3.Menus___forms.SecretarySubMenus
 
         }
 
-        private void Form_secretaryStudentRequests_Load(object sender, EventArgs e)
+        private void BackButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
