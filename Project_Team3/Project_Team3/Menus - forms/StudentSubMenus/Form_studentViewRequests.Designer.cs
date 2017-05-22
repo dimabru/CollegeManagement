40f82cd3284990fc78dd6a43d370d0a8111956fc
Alexey Silyuk <alexesi@ac.sce.ac.il>
2017-05-22 16:01:42 +0300

    added form for student view student requests

---
+++ Project_Team3/Project_Team3/Menus - forms/StudentSubMenus/Form_studentViewRequests.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Project_Team3.Menus___forms.StudentSubMenus
+{
+    partial class Form_studentViewRequests
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form_studentViewRequests
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 320);
+            this.Name = "Form_studentViewRequests";
+            this.Text = "Form_studentViewRequests";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
