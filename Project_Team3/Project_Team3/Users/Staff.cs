f1064f3fe47d6e0bb0c3b88a7b7b0fb4374730ce
dimab <dimabru@gmail.com>
2017-05-24 15:45:23 +0300

    create class Staff

---
+++ Project_Team3/Project_Team3/Users/Staff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Team3.Users
+{
+    class Staff
+    {
+
+        string firstName;
+        string lastName;
+        int id;
+
+        public Staff(int ID, string FIRSTNAME, string LASTNAME)
+        {
+            id = ID;
+            firstName = FIRSTNAME;
+            lastName = LASTNAME;
+        }
+
+        public string getFirstName() { return firstName; }
+
+        public string getLastName() { return lastName; }
+
+        public int getId() { return id; }
+
+    }
+}
